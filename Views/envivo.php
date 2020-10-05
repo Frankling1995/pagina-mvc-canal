4b13f1079d5099aaa73ee26e404185b25e9c5a1d
Frankling1995 <61323268+Frankling1995@users.noreply.github.com>
2020-10-05 16:14:32 -0400

    Creacion_contacto

---
+++ Views/envivo.php
@@ -0,0 +1,10 @@
+<?php include_once 'Views/include/links.php';?>
+<?php include_once 'Views/include/header.php';?>   
+<?php include_once 'Views/include/Nav.php';?>  
+
+<!------------------------------------------------>
+   
+
+<!------------------------------------------------>
+<?php include_once 'Views/include/footer.php';?>    
+<?php include_once 'Views/include/script.php';?>   
