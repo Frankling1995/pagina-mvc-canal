030750c201d591dd1383b621add19e826b8da0de
Frankling1995 <61323268+Frankling1995@users.noreply.github.com>
2020-10-14 23:42:07 -0400

    pratica_14_10_2020

--- Controllers/MediaController.php
+++ Controllers/MediaController.php
@@ -3,12 +3,21 @@ require_once 'Models/MediaModel.php';
 
 class MediaController{
 
+    public $tabla_name='media';
 
- public function Mediaid(){
-    $id=$_GET['id'];
-    $Model = new MediaModel();
-    $media=$Model->Get_media_id($id); 
-     echo json_encode($media); 
- }
+    public function Mediaid(){
+        $id=$_GET['id'];
+        $Model = new MediaModel();
+        $media=$Model->Get_media_id($id); 
+        echo json_encode($media); 
+    }
+
+    public function Media_all(){
+        $Model = new MediaModel();
+        $media=$Model->Get_all($this->tabla_name); 
+        echo json_encode($media); 
+
+    }
+
 
 }
--- Models/MediaModel.php
+++ Models/MediaModel.php
@@ -6,19 +6,24 @@ class MediaModel extends Model{
     private $id;
     private $url_media;
     private $tipo;
+    
 
     public function __GET($k){ return $this->$k; }
     public function __SET($k, $v){return $this->$k = $v;}
 
+
+// ASIGNA LOS VALORES CONTENIDOS EN $r AL OBJETO MODEL
     public function Set_Object($r){
 
     }
-
+    
+//BUSCAR EL DATA REFERENCIADO POR EL ID   
     public function Get_media_id($id){
+        $sql= "SELECT * FROM media WHERE id=? ";
         try 
 		{
 			$stm = $this->pdo
-                      ->prepare("SELECT * FROM media WHERE id=? ");
+                      ->prepare($sql);
 			$stm->execute(array($id));
 			$r = $stm->fetch(PDO::FETCH_OBJ);            
             return $r;
@@ -28,4 +33,7 @@ class MediaModel extends Model{
 			die($e->getMessage());
 		}
     }
+
+
+
 }
