d781f672632c78efb7bb0c7f3d33832d13b963e1
Frankling1995 <61323268+Frankling1995@users.noreply.github.com>
2020-10-19 01:43:27 -0400

    Usuario_model

--- Models/UsuarioModel.php
+++ Models/UsuarioModel.php
@@ -11,10 +11,28 @@ class UsuarioModdel extends Model{
     private $id_rol;
     private $fullname;
 
+//METODO CREATE 
+
+    public function Guardar(){
+        $sql="INSERT INTO  usuario (username,password,id_rol, fullname) VALUES (?,?,?,?) ";
+        
+        try {
+            //code...
+        } catch (Exception $e) {
+            //throw $th;
+        }
+        
+
+    }
+
+//METODO READ SEGUN UN USERNAME    
     public function Get_username($username){
-        $sql= "SELECT * FROM usuario WHERE  username=?";
+        $sql= "SELECT username, password, rol ,fullname  FROM usuario WHERE  username=?";
         try {
             $stm= $this->pdo->prepare($sql);
+            $stm->execute(array($username));
+            $r=$stm->fetch(PDO::FETCH_OBJ);
+            return $r;
 
         } catch (Exception $e) {
             return "Error SQL ";
