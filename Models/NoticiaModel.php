c1dd0d7b4b4f20d178b71d64985b0bcd861f9bf7
Frankling1995 <61323268+Frankling1995@users.noreply.github.com>
2020-11-02 16:18:38 -0400

    Edicion de media

--- Models/NoticiaModel.php
+++ Models/NoticiaModel.php
@@ -4,6 +4,37 @@ require_once 'Model/Mode.php';
 
 class NoticiaModel extends Model{
 
+//CAMPOS DE LA TABLA NOTICIA
+    private $id;
+    private $titulo;
+    private $contenido;
+    private $id_categoria;
+    private $id_media;
+    private $id_usuario;
+    private $fecha;
+//METODO GET Y SET 
+
+public function _GET($k){ return $this->$k; }
+public function _SET($k, $v){return $this->$k = $v;}
+
+//SETEA EL OBJETO MODELO CON LOS VALORES CONTENIDOS EN $r
+public function Set_Object($r){
+    
+    $this->id = $r['id'];
+    $this->titulo= $r['titulo'];
+    $this->contenido = $r['contenido'];
+    $this->id_categoria = $r['id_categoria'];
+    $this->id_media = $r['id_media'];
+    $this->id_usuario= $r['id_usuario'];
+    $this->fecha= $r['fecha'];
+}
+
+//SEIS NOTICIAS PRINCIPAL
+
+    public function  noticia_principal(){
+        $sql="SELECT  titulo, contenido, categoria , url_media, fullname, fecha  ";
+    
+
     
 
 
@@ -12,5 +43,7 @@ class NoticiaModel extends Model{
 
 
 
+
+}
 
 }
