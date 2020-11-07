6a33468c7403addcdd2c92601e229479b9bcbe60
Frankling1995 <61323268+Frankling1995@users.noreply.github.com>
2020-11-07 16:19:56 -0400

    metodo create NM

--- Models/NoticiamediaModel.php
+++ Models/NoticiamediaModel.php
@@ -11,6 +11,33 @@ class NoticiamediaModel extends Model {
 
     public function _GET($k){ return $this->$k; }
     public function _SET($k, $v){return $this->$k = $v;}
+//SETEA EL OBJETO MODELO CON LOS VALORES CONTENIDOS EN $r
+public function Set_Object($r){
+    $this->id_noti_media = $r['id_noti_media'];    
+    $this->media = $r['media'];
+    $this->media_2= $r['media_2'];
+    $this->media_3= $r['media_3'];
+}
+
+//METODO CREATE 
+
+    public function Guardar(){
+        $sql="INSERT INTO noticia_media (media,media_2,media_2) VALUES ?,?,?";
+        try {
+            $stm=$this->pdo->prepare($sql)
+            ->execute(
+            array(
+                $this->media,
+                $this->media_2,
+                $this->media_3
+            ));
+            $this->id= $this->pdo->lastInsertId();
+        } catch (Exception $e) {
+            return 'Error en el sql';
+        }
+
+
+    }
 
 
 
